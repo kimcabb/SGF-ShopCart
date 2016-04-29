73092f2dceadab7b6f2321ea794116151229a437
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-04-28 21:35:06 -0400

    added controller, cart and product views

--- js/components/Cart.js
+++ js/components/Cart.js
@@ -1,1 +1,53 @@
 // cart component
+
+var React = require('react');
+var CartActions = require('../actions/CartActions');
+
+// cart view
+var Cart = React.createClass({
+
+  // hide cart via actions
+  closeCart: function(){
+    CartActions.updateCartVisible(false);
+  },
+
+  // show cart via actions
+  openCart: function(){
+    CartActions.updateCartVisible(true);
+  },
+
+  // remove item from cart via actions
+  removeFromCart: function(sku){
+    CartActions.removeFromCart(sku);
+    CartActions.updateCartVisible(false);
+  },
+
+  // render cart view
+  render: function(){
+    var self = this, products = this.props.products;
+    return(
+      <div className={"cart " + (this.props.visible ? 'active' : '')}>
+        <div className="mini-cart">
+          <button type="button" className="close-cart" onClick={this.closeCart}>×</button>
+          <ul>
+            {Object.keys(products).map(function(product){
+              return(
+                <li key={product}>
+                  <h1 className="name">{products[product].name}</h1>
+                  <p className="type">{products[product].type} x {products[product].quantity}</p>
+                  <p className="price">${(products[product].price * products[product].quantity).toFixed(2)}</p>
+                  <button type="button" className="remove-item" onClick={self.removeFromCart.bind(self, product)}>Remove</button>
+                </li>
+              )
+            })}
+          </ul>
+          <span className="total">Total: ${this.props.total}</span>
+        </div>
+        <button type="button" className="view-cart" onClick={this.openCart} disabled={Object.keys(this.props.products).length > 0 ? "" : "disabled"}>View Cart ({this.props.count})</button>
+      </div>
+    );
+  },
+
+});
+
+module.exports = Cart;
--- js/components/CartApp.js
+++ js/components/CartApp.js
@@ -1,1 +1,58 @@
 // main controller view
+
+var React = require('react');
+var CartStore = require('../stores/CartStore');
+var ProductStore = require('../stores/ProductStore');
+var Product = require('./Product');
+var Cart = require('./Cart');
+
+// method to retrieve state from Stores
+function getCartState(){
+  return {
+    product: ProductStore.getProduct(),
+    selectedProduct: ProductStore.getSelected(),
+    cartItems: CartStore.getCartItems(),
+    cartCount: CartStore.getCartCount(),
+    cartTotal: CartStore.getCartTotal(),
+    cartVisible: CartStore.getCartVisible()
+  };
+}
+
+// define main controller view
+var FluxCartApp = React.createClass({
+
+  // get initial state from stores
+  getInitialState: function(){
+    return getCartState();
+  },
+
+  // add change listeners to stores
+  componentDidMount: function(){
+    ProductStore.addChangeListener(this._onChange);
+    CartStore.addChangeListener(this._onChange);
+  },
+
+  // remove change listeners from stores
+  componentWillUnmount: function(){
+    ProductStore.removeChangeListener(this._onChange);
+    CartStore.removeChangeListener(this._onChange);
+  },
+
+  // render child components, passing state via props
+  render: function(){
+    return(
+      <div className="cart-app">
+        <Cart products={this.state.cartItems} count={this.state.cartCount} total={this.state.cartTotal} visible={this.state.cartVisible} />
+        <Product product={this.state.product} cartitems={this.state.cartItems} selected={this.state.selectedProduct} />
+      </div>
+    );
+  },
+
+  // method to set state based upon Store changes
+  _onChange: function(){
+    this.setState(getCartState());
+  }
+
+});
+
+module.exports = CartApp;
